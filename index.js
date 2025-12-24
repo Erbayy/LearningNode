e5503f60c6aeca872aefcbf85904c0879d68800e
Erbay <erberkerbay6161@gmail.com>
2025-12-24 20:06:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.use(express.json());
 
+const users = [
+  { id: 1,name:"Erberk",role : "student"},
+  { id: 2,name:"Erbay",role : "student"},
+]
+
 app.get("/", (req, res) => {
   res.send("Backend calisiyor. Erberk basladi ✅");
 });
@@ -11,14 +16,29 @@ app.get("/health", (req, res) => {
   res.json({ status: "ok", time: new Date().toISOString() });
 });
 
-app.listen(3000, () => {
-  console.log("Server calisiyor: http://localhost:3000");
-});
-
 app.get("/about", (req, res) => {
   res.json({
     name: "Erberk",
     role: "Backend Developer (starting)",
     stack: ["Node.js", "Express"],
   });
+});
+
+app.get("/users/:id",(req,res) => {
+  const id = Number(req.params.id);
+  const user = users.find((u) => u.id === id);
+
+  if(!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  res.json(user);
+});
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.listen(3000, () => {
+  console.log("Server calisiyor: http://localhost:3000");
 });
